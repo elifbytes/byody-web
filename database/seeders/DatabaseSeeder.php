73f24d3bd94b12f6fd6d53fb20cb67ac88615545
Afif Muhammad <afif@carigi.id>
2025-08-09 08:31:57 +0700

    feat(docker): set up Docker environment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Lunar\Admin\Models\Staff;
+use Lunar\Models\Currency;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CurrencySeeder::class);
+        // $this->call(CurrencySeeder::class);
         // $this->call(CollectionSeeder::class);
         // $this->call(AttributeSeeder::class);
         // $this->call(TaxSeeder::class);
@@ -20,5 +23,26 @@ class DatabaseSeeder extends Seeder
         // $this->call(ShippingSeeder::class);
         // $this->call(CustomerSeeder::class);
         // $this->call(OrderSeeder::class);
+
+        DB::transaction(function () {
+            Staff::firstOrCreate([
+                'email' => 'admin@example.com',
+                'first_name' => 'Admin',
+                'last_name' => 'User',
+                'password' => bcrypt('admin123'),
+                'admin' => true,
+            ]);
+
+            if (! Currency::whereDefault(true)->exists()) {
+                Currency::create([
+                    'code' => 'IDR',
+                    'name' => 'Indonesian Rupiah',
+                    'exchange_rate' => 1,
+                    'decimal_places' => 0,
+                    'default' => true,
+                    'enabled' => true,
+                ]);
+            }
+        });
     }
 }
