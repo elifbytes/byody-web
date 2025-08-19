556059f2ed8fb1c859cf180ea60de0bdb4daf92b
Afif Muhammad <afif@carigi.id>
2025-08-19 15:55:03 +0700

    feat(auth): enhance login flow by storing referer for redirect after authentication

--- routes/auth.php
+++ routes/auth.php
@@ -6,11 +6,15 @@ use Laravel\WorkOS\Http\Requests\AuthKitLoginRequest;
 use Laravel\WorkOS\Http\Requests\AuthKitLogoutRequest;
 
 Route::get('login', function (AuthKitLoginRequest $request) {
+    $referer = $request->headers->get('referer');
+    session(['redirect_url' => $referer]);
     return $request->redirect();
 })->middleware(['guest'])->name('login');
 
 Route::get('authenticate', function (AuthKitAuthenticationRequest $request) {
-    return tap(to_route('home'), fn () => $request->authenticate());
+    return tap(redirect(
+        session('redirect_url', '/')
+    ), fn() => $request->authenticate());
 })->middleware(['guest']);
 
 Route::post('logout', function (AuthKitLogoutRequest $request) {
